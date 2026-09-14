93ea7cf18d93b6964c9e3036407e0940f2c8c900
kparpinelli24-sys <k.parpinelli.24@gmail.com>
2026-09-14 09:47:20 -0300

    Implement MQTT client message handling and connection status updates

--- dashoard.js
+++ dashoard.js
@@ -5,4 +5,27 @@ const client = new Paho.MQTT.Client(
     broker,
     porta,
     clientId
-);
+); 
+const status = document.getElementById("status");
+const temperatura = document.getElementById("temperatura");
+const umidade = document.getElementById("umidade");
+const qualidadeAr = document.getElementById("qualidade-ar");
+
+client.onConnectionLost = function () {
+    status.textContent = "● Desconectado";
+    status.className = "status desconectado";
+};
+
+client.onMessageArrived = function (message) {
+    const topico = message.destinationName;
+    const valor = message.payloadString;
+    if (topico === "aulas/professortupi/temperatura") {
+        temperatura.textContent = Number(valor).toFixed(1) + " °C";
+    }
+    if (topico === "aulas/professortupi/umidade") {
+        umidade.textContent = Number(valor).toFixed(1) + " %";
+    }
+    if (topico === "aulas/professortupi/qualidade_ar") {
+        qualidadeAr.textContent = valor;
+    }
+};
